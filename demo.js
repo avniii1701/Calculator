b5253df75887d9612fad42ad41688ed7ade66db2
avniii1701 <avniii1710@gmail.com>
2025-02-14 22:38:20 +0530

    demo.js

--- demo.js
+++ demo.js
@@ -19,3 +19,38 @@ function evaluateExpression() {
         alert("Invalid Expression");
     }
 }
+
+function square() {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = Math.pow(value, 2);
+}
+
+function squareRoot() {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = Math.sqrt(value);
+}
+
+function cube() {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = Math.pow(value, 3);
+}
+
+function cubeRoot() {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = Math.cbrt(value);
+}
+
+function factorial() {
+    let value = parseInt(document.getElementById("result").value);
+    if (value < 0) {
+        alert("Factorial of negative numbers is not defined");
+        return;
+    }
+    let fact = 1;
+    for (let i = 1; i <= value; i++) {
+        fact *= i;
+    }
+    document.getElementById("result").value = fact;
+}
+
+
